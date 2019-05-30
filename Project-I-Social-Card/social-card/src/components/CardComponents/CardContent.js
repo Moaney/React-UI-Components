3f2891922e582aa82ba2dd21faf356dee4f19f6a
Moaney <ladonnamoney@gmail.com>
2019-05-29 20:44:16 -0400

    updated components and imported to app.js for social card

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return (
+        <div className='CardContent'>
+            <h3>Get Started with React</h3>
+            <p>React makes it paineless to create interactive UIs. Design simple views for each state in your appication</p>
+            <a>reactjs.org</a>
+        </div>
+    );
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return <h3>Lamda School @Lamda School * 5/29</h3>;
+}
+
+export default HeaderTitle;
